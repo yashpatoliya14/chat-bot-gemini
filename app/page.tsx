499d25a3a4750e3ae2b5c9cdd7ab4dc37f903042
YASH PATOLIYA <yashpatoliya14@gmail.com>
2025-12-01 19:06:52 +0530

    add

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ import Dashboard from '@/components/dashboard/Dashboard'
 export default function Home() {
   return (
     <>
-      <Dashboard/>
+      <Dashboard/> 
+      
     </>
   );
 }
--- components/dashboard/dashboard.tsx
+++ components/dashboard/dashboard.tsx
@@ -68,7 +68,7 @@ export default function Dashboard() {
       sx={{
         display: "flex",
         flexDirection: "column",
-        minHeight: "100vh",
+        minHeight: "99vh",
         bgcolor: "background.default",
       }}
     >
